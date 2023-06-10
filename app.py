14a225070975381fbb37d14747c9aded2cfb4e03
Pranith2628 <pranithreddymamidi@gmail.com>
2023-06-10 22:06:59 +0530

    commit

--- app.py
+++ app.py
@@ -11,88 +11,97 @@ ma = Marshmallow()
 mysql = MySQL(app)
 
 
-class Product(db.Model):
+class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(15), nullable=False)
-    price = db.Column(db.Float, nullable=False)
-    category = db.Column(db.String(10), nullable=False)
+    title = db.Column(db.String(15), nullable=False)
+    description = db.Column(db.String(30), nullable=False)
+    due_date = db.Column(db.Date, nullable=False)
+    status = db.Column(db.String(30), nullable=False)
 
-    def __int__(self, name, price, category):
-        self.name = name
-        self.price = price
-        self.category = category
+    def __int__(self, title, description, due_date, status):
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        self.status = status
 
 
-class ProductSchema(ma.Schema):
+class TaskSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'price', 'category')
+        fields = ("id", "title", "description", "due_date", "status")
 
 
-product_schema = ProductSchema()
-products_schema = ProductSchema(many=True)
+task_schema = TaskSchema()
+tasks_schema = TaskSchema(many=True)
 
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:""@localhost/product"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:@localhost/task"
 db.init_app(app)
 with app.app_context():
     db.create_all()
 
+
 # 'POST' = creating new data
-@app.route('/product/add', methods=['POST'])
-def add_product():
+@app.route('/task', methods=['POST'])
+def add_task():
     _json = request.json
-    name = _json['name']
-    price = _json['price']
-    category = _json['category']
-    new_product = Product(name=name, price=price, category=category)
-    db.session.add(new_product)
+    title = _json['title']
+    description = _json['description']
+    due_date = _json['due_date']
+    status = _json['status']
+    new_task = Task(title=title, description=description, due_date=due_date, status=status)
+    db.session.add(new_task)
     db.session.commit()
-    return jsonify({"message": "your product has been successfully added"})
+    return jsonify({"Message": "your Task has been added successfully"})
 
-# 'GET' = reading all the data from the table
-@app.route('/product', methods=['GET'])
-def get_product():
-    products = []
-    data = Product.query.all()
-    products = products_schema.dump(data)
-    return jsonify(products)
 
-# 'GET' = reading a single product from the table by using <id>
-@app.route('/product/<id>', methods=['GET'])
-def get_product_byid(id):
+# 'GET' = reading a single task from the table by using <id>
+@app.route('/task/<id>', methods=['GET'])
+def task_byid(id):
     if not str.isdigit(id):
-        return jsonify(f"Message: The id of the product cannot be a string")
+        return jsonify(f"Message: The id of the task cannot be a string")
     else:
         data = []
-        product = Product.query.get(id)
+        product = Task.query.get(id)
         if product is None:
-            return jsonify(f"No product was found")
+            return jsonify(f"No task was found")
         else:
-            data = product_schema.dump(product)
+            data = task_schema.dump(product)
             return jsonify(data)
 
-# 'PUT' = updating a single product from the table by using <id>
-@app.route('/product/update/<id>', methods=['PUT'])
-def update_product_by_id(id):
-    data = request.get_json()
-    product = Product.query.get(id)
-    if data.get('name'):
-        product.name = data['name']
-    if data.get('price'):
-        product.price = data['price']
-    if data.get('category'):
-        product.category = data['category']
-    db.session.add(product)
-    db.session.commit()
-    return jsonify({"message": "your product has been successfully updated"})
 
-# 'DELETE' = deleting a single product from the table by using <id>
-@app.route('/product/delete/<id>', methods=['DELETE'])
-def delete_product_by_id(id):
-    product = Product.query.get(id)
-    db.session.delete(product)
+# 'PUT' = updating a single task from the table by using <id>
+@app.route('/task/update/<id>', methods=['PUT'])
+def update_task_by_id(id):
+    data = request.get_json()
+    task = Task.query.get(id)
+    if data.get('title'):
+        task.name = data['title']
+    if data.get('description'):
+        task.price = data['description']
+    if data.get('due_date'):
+        task.category = data['due_date']
+    if data.get('status'):
+        task.status = data['status']
+    db.session.add(task)
     db.session.commit()
-    return jsonify(f"The product has been deleted")
+    return jsonify({"message": "your task has been successfully updated"})
+
+
+# 'DELETE' = deleting a single task from the table by using <id>
+@app.route('/task/delete/<id>', methods=['DELETE'])
+def delete_task_by_id(id):
+    task = Task.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
+    return jsonify(f"The task has been deleted")
+
+
+# 'GET' = reading all the data from the table
+@app.route('/tasks', methods=['GET'])
+def get_task():
+    tasks = []
+    data = Task.query.all()
+    tasks = tasks_schema.dump(data)
+    return jsonify(tasks)
 
 
 if __name__ == "__main__":
